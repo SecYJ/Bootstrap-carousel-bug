8833f7a50ce21efc58f43b1ebfe639e0d9c99efc
SecYJ <qianqian1903@gmail.com>
2021-09-08 18:21:10 +0800

    carousel bug

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./index.scss";
+import Carousel from "bootstrap/js/dist/carousel";
+
+const carouselHandler = () => {
+	// const carousel = document.querySelector("#carouselExampleControls");
+	// const prevBtn = document.querySelector("#prevBtn");
+	// const nextBtn = document.querySelector("#nextBtn");
+	// const carouselInstance = new Carousel(carousel, {
+	// 	ride: "carousel",
+	// 	interval: 2000,
+	// 	// pause: false,
+	// });
+	// console.log(carouselInstance);
+	// prevBtn.addEventListener("click", () => carouselInstance.prev());
+	// nextBtn.addEventListener("click", () => carouselInstance.next());
+};
+
+// carouselHandler();
+console.log(prevBtn);
